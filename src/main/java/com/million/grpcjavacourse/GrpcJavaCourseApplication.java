90bd86ea347dfcfe2c60ec38cc93301bd42763b4
John Million <john.million@rtslabs.com>
2022-11-28 19:41:04 -0500

    Initial Commit

---
+++ src/main/java/com/million/grpcjavacourse/GrpcJavaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.million.grpcjavacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcJavaCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrpcJavaCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/million/grpcjavacourse/GrpcJavaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.million.grpcjavacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcJavaCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
