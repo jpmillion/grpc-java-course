c9a068ebb58593cc7074a8390d921c54eae46837
John Million <john.million@rtslabs.com>
2022-12-02 13:46:49 -0500

    Define Greeting server and client boiler plate

---
+++ src/main/java/com/million/grpcjavacourse/greeting/client/GreetingClient.java
@@ -0,0 +1,20 @@
+package com.million.grpcjavacourse.greeting.client;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class GreetingClient {
+
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder
+                .forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+
+
+        log.info("Shutting down");
+        channel.shutdown();
+    }
+}
---
+++ src/main/java/com/million/grpcjavacourse/greeting/server/GreetingServer.java
@@ -0,0 +1,28 @@
+package com.million.grpcjavacourse.greeting.server;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+
+@Slf4j
+public class GreetingServer {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int port = 50051;
+
+        Server server = ServerBuilder.forPort(port).build();
+
+        server.start();
+        log.info("Server started, listening on port: {}", port);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            log.info("Received Shutdown request");
+            server.shutdown();
+            log.info("Server stopped");
+        }));
+
+        server.awaitTermination();
+    }
+}
