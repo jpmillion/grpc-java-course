11417145eda923ec0391c0b27fbdc5669e4d0a29
John Million <john.million@rtslabs.com>
2022-12-06 21:39:20 -0500

    Define GreetingClient

--- src/main/java/com/million/grpcjavacourse/greeting/client/GreetingClient.java
+++ src/main/java/com/million/grpcjavacourse/greeting/client/GreetingClient.java
@@ -1,5 +1,8 @@
 package com.million.grpcjavacourse.greeting.client;
 
+import com.proto.greeting.GreetingRequest;
+import com.proto.greeting.GreetingResponse;
+import com.proto.greeting.GreetingServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import lombok.extern.slf4j.Slf4j;
@@ -8,13 +11,38 @@ import lombok.extern.slf4j.Slf4j;
 public class GreetingClient {
 
     public static void main(String[] args) {
+
+        if (args.length == 0) {
+            log.info("Need one argument to work");
+            return;
+        }
+
         ManagedChannel channel = ManagedChannelBuilder
                 .forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
 
+        switch (args[0]) {
+
+            case "greet":
+                doGreet(channel);
+                break;
+
+            default:
+                log.info("Keyword Invalid: " + args[0]);
+        }
 
         log.info("Shutting down");
         channel.shutdown();
+    }
+
+    private static void doGreet(ManagedChannel channel) {
+
+        log.info("Enter doGreet");
+
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+        GreetingResponse response = stub.greet(GreetingRequest.newBuilder().setFirstName("John").build());
+
+        log.info("Greeting: {}", response.getResult());
     }
 }
